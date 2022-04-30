e12cfd4b702e65917162e2fe3ce19e93f5378e8c
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2022-04-30 08:27:53 +0800

    Add files via upload

---
+++ 启动虚拟机测试.bat
@@ -0,0 +1,2 @@
+@echo off
+D:\Software\VMware\vmrun.exe start "D:\ÎÄµµ\Virtual Machines\Windows 10 x64\Windows 10 x64.vmx" gui
---
+++ 生成ISO.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "%~dp0"
+del %~dp0TempFile\ISO\HotPE\Kernel.wim /F /S /Q
+copy %~dp0Kernel.wim %~dp0TempFile\ISO\HotPE\Kernel.wim
+%~dp0Bin\Tools\oscdimg.exe  -m -o -u2 -udfver102 -h -bootdata:2#p0,e,b%~dp0Bin\Tools\Etfsboot.com#pEF,e,b%~dp0Bin\Tools\Efisys.bin %~dp0TempFile\ISO\ %~dp0HotPEπ§æﬂœ‰.iso
